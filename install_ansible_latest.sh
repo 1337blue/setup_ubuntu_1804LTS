381b53800f878ef2a405ed8a791fe7d9d715bc36
Phillip L <46860631+1337blue@users.noreply.github.com>
2019-05-03 17:20:46 +0200

    Moved to apt package install

--- install_ansible_latest.sh
+++ install_ansible_latest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y python3-pip
-pip install --upgrade pip
-pip install ansible
-pip install --upgrade setuptools
+apt-get update && apt-get install -y software-properties-common && \
+apt-add-repository --yes --update ppa:ansible/ansible && \
+apt-get update && apt-get upgrade -y && apt-get install -y ansible python-dev python-pip
 
-ansible-playbook base_installation.yml 
+ansible-playbook base_installation.yml
